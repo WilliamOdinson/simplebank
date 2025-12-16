af55ed5facdb0aa02117553f13782fe755bb068a
William Sun <109450999+WilliamOdinson@users.noreply.github.com>
2025-12-17 07:37:24 +0800

    feat(util): add viper config loading from environment file

--- main.go
+++ main.go
@@ -6,18 +6,19 @@ import (
 
 	"github.com/WilliamOdinson/simplebank/api"
 	db "github.com/WilliamOdinson/simplebank/db/sqlc"
+	"github.com/WilliamOdinson/simplebank/util"
 	"github.com/jackc/pgx/v5/pgxpool"
-)
-
-const (
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
 )
 
 func main() {
 	ctx := context.Background()
+	config, err := util.LoadConfig(".")
 
-	pool, err := pgxpool.New(ctx, dbSource)
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	pool, err := pgxpool.New(ctx, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -26,8 +27,8 @@ func main() {
 	store := db.NewStore(pool)
 	server := api.NewServer(store)
 
-	log.Printf("Starting server at %s", serverAddress)
-	err = server.Start(serverAddress)
+	log.Printf("Starting server at %s", config.ServerAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
