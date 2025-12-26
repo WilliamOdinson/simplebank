cf076c4ef0a1f2d1aa4016914a6774d44b75c000
William Sun <109450999+WilliamOdinson@users.noreply.github.com>
2025-12-26 18:09:14 +0800

    feat(api): enhance createTransfer to validate account ownership and improve error handling

--- api/transfer.go
+++ api/transfer.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	db "github.com/WilliamOdinson/simplebank/db/sqlc"
+	"github.com/WilliamOdinson/simplebank/token"
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,15 +20,29 @@ type transferRequest struct {
 func (server *Server) createTransfer(ctx *gin.Context) {
 	var req transferRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+
 	// Check if from and to account is valid
-	if !server.validAccount(ctx, req.FromAccountID, req.Currency) {
+	fromAccount, valid := server.validAccount(ctx, req.FromAccountID, req.Currency)
+	if !valid {
+		err := fmt.Errorf("from account %d is not valid", req.FromAccountID)
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
-	if !server.validAccount(ctx, req.ToAccountID, req.Currency) {
+
+	if fromAccount.Owner != authPayload.Username {
+		err := fmt.Errorf("from account %d does not belong to the authenticated user", req.FromAccountID)
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
+	if _, valid := server.validAccount(ctx, req.ToAccountID, req.Currency); !valid {
+		err := fmt.Errorf("to account %d is not valid", req.ToAccountID)
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 
@@ -39,7 +54,7 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 
 	result, err := server.store.TransferTx(ctx, arg)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
 
@@ -47,21 +62,21 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 }
 
 // validAccount checks if the account with given ID exists and if its currency matches the provided one.
-func (server *Server) validAccount(ctx *gin.Context, accountID int64, currency string) bool {
+func (server *Server) validAccount(ctx *gin.Context, accountID int64, currency string) (db.Account, bool) {
 	account, err := server.store.GetAccount(ctx, accountID)
 	if err == sql.ErrNoRows {
 		ctx.JSON(http.StatusNotFound, errorResponse(err))
-		return false
+		return account, false
 	} else if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-		return false
+		return account, false
 	}
 
 	if account.Currency != currency {
 		err := fmt.Errorf("account ID %d currency mismatch: expected %s, got %s", accountID, currency, account.Currency)
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-		return false
+		return account, false
 	}
 
-	return true
+	return account, true
 }
